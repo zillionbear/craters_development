a01a922d54c82ee1111ceba32c150c660f1fede5
zillionbear <72274542+zillionbear@users.noreply.github.com>
2020-12-16 21:11:34 -0800

    Initial commit
    +dotconnect

---
+++ dotconnect/dotconnect/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  dotconnect
+//
+//  Created by Zillionbear on 12/16/20.
+//  Copyright © 2020 dotconnect development. All rights reserved.
+//
+//  Contributers: Zillionbear (credit: idea and base app) [Your name could be here!]
+
+import UIKit
+
+class ViewController: UIViewController, UISearchBarDelegate, UIWebViewDelegate {
+    
+    @IBOutlet weak var SearchBar: UISearchBar!
+    @IBOutlet weak var WebView: UIWebView!
+    
+    @IBAction func backButton(_ sender: Any)
+    {
+        if WebView.canGoBack
+        {
+            WebView.goBack()
+        }
+    }
+    
+    @IBAction func refreshButton(_ sender: Any)
+    {
+        WebView.reload()
+    }
+    
+    @IBAction func forwardButton(_ sender: Any)
+    {
+        if WebView.canGoForward
+        {
+            WebView.goForward()
+        }
+    }
+    
+    @IBAction func stopNav(_ sender: Any)
+    {
+        WebView.loadRequest(URLRequest(url: URL(string: "about:blank")!))
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar)
+    {
+        SearchBar.resignFirstResponder()
+        
+        if let url = URL(string: SearchBar.text!)
+        {
+            WebView.loadRequest(URLRequest(url: url))
+        }
+    }
+    
+    func webViewDidStartLoad(_ webView: UIWebView)
+    {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView)
+    {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        WebView.loadRequest(URLRequest(url: URL(string: "https://www.google.com")!))
+    }
+
+
+}
+
---
+++ dotconnect/dotconnectTests/dotconnectTests.swift
@@ -0,0 +1,34 @@
+//
+//  dotconnectTests.swift
+//  dotconnectTests
+//
+//  Created by Arturo Cano on 12/16/20.
+//  Copyright © 2020 dotconnect development. All rights reserved.
+//
+
+import XCTest
+@testable import dotconnect
+
+class dotconnectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

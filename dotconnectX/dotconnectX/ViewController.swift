de10c1c2e2c83076bf5de12d068178266751517e
zillionbear <72274542+zillionbear@users.noreply.github.com>
2021-04-11 19:23:07 -0700

    trying to add dotconnectx via webupload
    trying to add dotconnectx via webupload because the desktop app is being an a**hole right now

---
+++ dotconnectX/dotconnectX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dotconnectX
+//
+//  Created by Arturo Cano on 4/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

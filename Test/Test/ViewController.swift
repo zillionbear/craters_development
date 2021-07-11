b53c523630c3d6f1272f7d792942fd6f2a39ac29
zillionbear <72274542+zillionbear@users.noreply.github.com>
2021-07-11 13:25:09 -0700

    Test Xcode
    Initial commit for Xcode project 'Test'

---
+++ Test/Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Arturo Cano on 7/11/21.
+//  Copyright © 2021 dotconnect development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
